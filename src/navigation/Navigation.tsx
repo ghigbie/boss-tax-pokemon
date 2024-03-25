5f2a5a0abf34642bef04f6b23caeec9495ca885e
George Higbie <georgehigbie@Georges-MBP.hsd1.il.comcast.net>
2024-03-24 21:07:42 -0500

    Edit navigator again

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,24 +1,17 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AllPokemon from '../screens/AllPokemon';
 import DetailScreen from '../screens/DetailScreen';
-
 
 const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home"  screenOptions={{ headerShown: true}}>
-        <Stack.Screen
-          name="Home"
-          component={AllPokemon}
-        />
-        <Stack.Screen
-          name="Detail"
-          component={DetailScreen}
-        />
+      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: true }}>
+        <Stack.Screen name="Home" component={AllPokemon} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
