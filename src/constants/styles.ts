7e8998e40a45c687a35c6c9256e153e00d3821ba
George Higbie <georgehigbie@Georges-MBP.hsd1.il.comcast.net>
2024-03-25 19:50:36 -0500

    Add styles for constants

---
+++ src/constants/styles.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+    primary: '#FFA500', // Orange
+    text: '#FFFFFF', // White
+    accent: '#00CED1', // Dark turquoise
+    error: '#FF0000', // Red
+    success: '#32CD32', // Green
+    warning: '#FFD700', // Yellow
+    darkGrey: '#383838', // Darkgrey
+    black: '#000000' // Black
+
+}
